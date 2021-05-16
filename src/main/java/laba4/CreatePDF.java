1b6a68e615b155d1e2a912dbdcb342ba22e0878d
User <User@fErrguzz-PC>
2021-05-16 18:12:36 +0500

    aboba

--- src/main/java/laba4/CreatePDF.java
+++ src/main/java/laba4/CreatePDF.java
@@ -32,7 +32,7 @@ public class CreatePDF {
 			for(int i=0;i<(lengthpath-1);i++) {
 				abspath=abspath+parsfilepath[i]+"/";
 			}
-			filepath=abspath+"webapps/Calculate/Check.pdf";
+			filepath=abspath+"src/main/webapp/Check.pdf";
 			String fontpath =abspath+"/webapps/CreatePDF/fonts/times.ttf";
 	    	
 			try {	
